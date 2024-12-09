e97a1a19f0935c5e87f4cccbf3f418adb5b8ef90
isabelrem <42750792+isabelrem@users.noreply.github.com>
2024-12-09 13:57:27 +0000

    Update tests_fhprs.py

--- test/tests_fhprs.py
+++ test/tests_fhprs.py
@@ -7,7 +7,7 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from fh import PRS
 
-class TestReadVCF(unittest.TestCase):
+class FHPRS_tests(unittest.TestCase):
 
     @patch("builtins.open",side_effect=FileNotFoundError) # generating a mocked error
     def test_file_not_found(self, mock_open):
@@ -139,13 +139,5 @@ class TestReadVCF(unittest.TestCase):
         
 
 
- 
-        
-
-
-
-
-
-# test a vcf with totally wild values idk
 
         
